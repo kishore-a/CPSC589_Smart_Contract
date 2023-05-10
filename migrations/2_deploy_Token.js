49915e7d38ebef91cf56cbde2a9118b86be49ef8
KISHORESHANKAR20062000 <60849170+KISHORESHANKAR20062000@users.noreply.github.com>
2023-05-10 03:42:54 -0700

    Initial Commit

---
+++ migrations/2_deploy_Token.js
@@ -0,0 +1,5 @@
+const MyToken = artifacts.require('MyToken')
+module.exports = async function (deployer, network, accounts) {
+  await deployer.deploy(MyToken)
+  const myToken = await MyToken.deployed()
+}
