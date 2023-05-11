472f681e642fd2f6cd8d7a0b3c8f48ce6bf1483e
KISHORESHANKAR20062000 <60849170+KISHORESHANKAR20062000@users.noreply.github.com>
2023-05-11 03:22:38 -0700

    Added Deposit and transaction Functions

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -3,6 +3,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
     constructor() public ERC20("MyToken", "MTKN") {
-        _mint(msg.sender, 1000000000000000000000000);
+        _mint(msg.sender, 10000);
     }
 }
--- migrations/2_deploy_Token.js
+++ migrations/2_deploy_Token.js
@@ -1,5 +1,8 @@
-const MyToken = artifacts.require('MyToken')
+const MyToken = artifacts.require("MyToken")
+const FarmToken = artifacts.require("FarmToken")
 module.exports = async function (deployer, network, accounts) {
   await deployer.deploy(MyToken)
   const myToken = await MyToken.deployed()
+  await deployer.deploy(FarmToken, myToken.address)
+  const farmToken = await FarmToken.deployed()
 }
